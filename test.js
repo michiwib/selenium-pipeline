8169c412f5f2bd7b5f4cb3f75085fead92b43351
unknown <michaeltunes80@gmail.com>
2019-05-18 19:36:07 -0400

    test selenium testing with jenkins

---
+++ test.js
@@ -0,0 +1,27 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+var assert = require('chai').assert;
+
+
+describe("test", async () => {
+    it("should perform a test", async () => {
+        let driver = await new Builder().forBrowser('chrome').build();
+        try {
+            await driver.get('http://www.google.com/ncr');
+            await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+            await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+        } finally {
+            await driver.quit();
+        }
+    })
+})
+
+// (async function example() {
+//   let driver = await new Builder().forBrowser('chrome').build();
+//   try {
+//     await driver.get('http://www.google.com/ncr');
+//     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+//     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+//   } finally {
+//     await driver.quit();
+//   }
+// })();
