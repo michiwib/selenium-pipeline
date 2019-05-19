e6f93af5458486259e60b20578c97ce8b3b002f0
unknown <michaeltunes80@gmail.com>
2019-05-19 15:25:37 -0400

    adding test log

--- test.js
+++ test.js
@@ -14,12 +14,14 @@ const capabilities = {
 
 describe("test", async () => {
     it("should perform a test", async () => {
-        // let driver = await new Builder().forBrowser('chrome').build();
-        let driver = await new Builder().usingServer("http://10.129.54.76:5555/wd/hub").withCapabilities(capabilities).build();
+        let driver = await new Builder().forBrowser('chrome').build();
+        //let driver = await new Builder().usingServer("http://10.129.54.76:5555/wd/hub").withCapabilities(capabilities).build();
         try {
             await driver.get('http://www.google.com/ncr');
             await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
             await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+            var loggedTitle = await driver.getTitle();
+            console.log(loggedTitle);
         } finally {
             await driver.quit();
         }
