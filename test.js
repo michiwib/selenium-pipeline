a71a982662f33513524349b7f79609b852a478aa
unknown <michaeltunes80@gmail.com>
2019-05-19 15:19:25 -0400

    trying out hub

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 var assert = require('chai').assert;
 
 const capabilities = {
-    platform: 'windows 10',
+    platform: 'LINUX',
     browserName: 'chrome',
     version: '74.0',
     resolution: '1280x800',
@@ -14,8 +14,8 @@ const capabilities = {
 
 describe("test", async () => {
     it("should perform a test", async () => {
-        let driver = await new Builder().forBrowser('chrome').build();
-        // let driver = await new Builder().usingServer("http://10.129.54.76/wd/hub").withCapabilities(capabilities).build();
+        // let driver = await new Builder().forBrowser('chrome').build();
+        let driver = await new Builder().usingServer("http://10.129.54.76:5555/wd/hub").withCapabilities(capabilities).build();
         try {
             await driver.get('http://www.google.com/ncr');
             await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
