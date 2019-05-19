fcd7fc4f7b1d1561e803c38ce4c0ec0e0d61fdbf
unknown <michaeltunes80@gmail.com>
2019-05-19 14:03:45 -0400

    changed webdriver versions

--- test.js
+++ test.js
@@ -1,10 +1,21 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 var assert = require('chai').assert;
 
+const capabilities = {
+    platform: 'windows 10',
+    browserName: 'chrome',
+    version: '74.0',
+    resolution: '1280x800',
+    network: true,
+    visual: true,
+    console: true,
+    video: true
+}
 
 describe("test", async () => {
     it("should perform a test", async () => {
         let driver = await new Builder().forBrowser('chrome').build();
+        // let driver = await new Builder().usingServer("http://10.129.54.76/wd/hub").withCapabilities(capabilities).build();
         try {
             await driver.get('http://www.google.com/ncr');
             await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
